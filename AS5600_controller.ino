b53111db33010de03ae70efc5f69a7ca00dd9e39
Wagner Santos Pereira da Silva <wagnerspse@gmail.com>
2023-08-14 22:08:39 -0300

    Codigo final, posiciona o motor atraves do angulo de feedback do sensor AS5600

--- AS5600_controller.ino
+++ AS5600_controller.ino
@@ -1,38 +1,157 @@
 #include <Wire.h>
+#define SENSOR_ADDRESS 0x36
+#define DIR_PIN 2
+#define STEP_PIN 3
 
-#define AS5600_ADDR 0x36  // Endereço I2C do sensor AS5600
+float stepsPerDegree = 0.0879;
+bool isMotorMoving = false;
+
+int initialPosition = 0;
+int actualPosition = 0;
+
+float initialAngle = 0;
+float actualAngle = 0;
+int desiredAngle = 0;
 
 void setup() {
+
   Wire.begin();
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
   Serial.begin(9600);
 
-  // Configuração inicial do AS5600
-  Wire.beginTransmission(AS5600_ADDR);
-  Wire.write(0x0B);  // Endereço do registro para configurar a resolução (12 bits)
-  Wire.write(0x0C);  // Valor para configurar a resolução
-  Wire.endTransmission();
+  Wire.beginTransmission(SENSOR_ADDRESS);
+  Wire.write(0x0E);
+  Wire.endTransmission(false);
+  Wire.requestFrom(SENSOR_ADDRESS, 2);
+
+  initialPosition = Wire.read() << 8 | Wire.read();
+  initialAngle = calculateCurrentAngle(initialPosition);
+
+  actualPosition = initialPosition;
+  actualAngle = initialAngle;
+  desiredAngle = initialAngle;
 }
 
 void loop() {
-  // Realiza leitura do sensor AS5600
-  Wire.beginTransmission(AS5600_ADDR);
-  Wire.write(0x0E);  // Endereço do registro para ler a posição angular
-  Wire.endTransmission();
 
-  Wire.requestFrom(AS5600_ADDR, 2);  // Solicita 2 bytes de dados
-  if (Wire.available() >= 2) {
-    int msb = Wire.read();
-    int lsb = Wire.read();
-    int position = (msb << 8) | lsb;  // Combina os bytes lidos
+  if (Serial.available() > 0) {
+    desiredAngle = Serial.parseInt();
+    desiredAngle = constrain(desiredAngle, 0, 360);
+    isMotorMoving = true;
+  }
 
-    // Calcula a posição absoluta em graus (considerando 360 graus completos)
-    float absolutePosition = position * 360.0 / 4096.0;
+  int currentPosition = getCurrentPosition();
+  float currentAngle = calculateCurrentAngle(currentPosition);
 
-    // Exibe a posição absoluta no monitor serial
-    Serial.print("Posição absoluta: ");
-    Serial.print(absolutePosition);
-    Serial.println(" graus");
+  Serial.print("Angulo atual: ");
+  Serial.print(currentAngle, 2);
+  Serial.println(" graus");
+
+  Serial.print("Angulo desejado: ");
+  Serial.print(desiredAngle);
+  Serial.println(" graus");
+
+  if (isMotorMoving) {
+    int currentPosition = getCurrentPosition();
+    int desiredPosition = calculateDesiredPosition(desiredAngle);
+
+    Serial.println("-------------------");
+    Serial.print("Posicao atual: ");
+    Serial.println(currentPosition);
+    Serial.print("Posicao desejado: ");
+    Serial.println(desiredPosition);
+    Serial.println("-------------------");
+
+    if (((currentPosition - desiredPosition) > 5) || ((desiredPosition - currentPosition) > 5)) {
+      moveMotorToPosition(desiredPosition);
+    } else {
+      stopMotor();
+      isMotorMoving = false;
+    }
   }
 
   delay(500);
+}
+
+int calculateDesiredPosition(int angle) {
+  return map(angle, 0, 360, 0, 4095);
+}
+
+
+void moveMotorToPosition(int desiredPosition) {
+
+  Serial.println("---- ATIVANDO MOTOR ------- ");
+
+  int currentPosition = getCurrentPosition();
+
+  Serial.print("Angulo atual: ");
+  Serial.println(actualAngle);
+  Serial.print("Angulo desejada: ");
+  Serial.println(desiredAngle);
+
+  Serial.print("Posicao atual: ");
+  Serial.println(currentPosition);
+  Serial.print("Posicao desejada: ");
+  Serial.println(desiredPosition);
+
+  int direction = (desiredPosition > currentPosition) ? HIGH : LOW;
+  digitalWrite(DIR_PIN, direction);
+
+  Serial.print("Direcao: ");
+  Serial.println((desiredPosition > currentPosition) ? "Horario" : "Anti-Horario");
+
+  int steps = 0;
+
+  if (desiredPosition > currentPosition) {
+    steps = abs((desiredPosition - currentPosition) * 0.788177339);
+  } else {
+    steps = abs((currentPosition - desiredPosition) * 0.788177339);
+  }
+
+  Serial.print("Numero de passos: ");
+  Serial.println(steps);
+
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(200);
+    currentPosition = getCurrentPosition();
+  }
+
+  actualPosition = getCurrentPosition();
+  actualAngle = calculateCurrentAngle(actualPosition);
+
+  Serial.println("------------------ ");
+
+  Serial.print("Angulo atual: ");
+  Serial.println(actualAngle);
+  Serial.print("Angulo desejada: ");
+  Serial.println(desiredAngle);
+
+  Serial.print("Posicao atual: ");
+  Serial.println(actualPosition);
+  Serial.print("Angulo atual: ");
+  Serial.println(actualAngle, 2);
+
+  Serial.println("------ FIM ------- ");
+}
+
+void stopMotor() {
+  digitalWrite(STEP_PIN, LOW);
+}
+
+int getCurrentPosition() {
+  Wire.beginTransmission(SENSOR_ADDRESS);
+  Wire.write(0x0E);
+  Wire.endTransmission(false);
+  Wire.requestFrom(SENSOR_ADDRESS, 2);
+  int currentPosition = Wire.read() << 8 | Wire.read();
+  return currentPosition;
+}
+
+float calculateCurrentAngle(int currentPosition) {
+  float currentAngle = map(currentPosition, 0, 4095, 0, 360);
+  return currentAngle;
 }
